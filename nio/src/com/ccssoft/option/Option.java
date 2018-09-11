91a86da51e8264594b6e2033802c9923852d2b8d
CC <1176652520@qq.com>
2018-09-11 14:13:21 +0800

    初始化netty

---
+++ nio/src/com/ccssoft/option/Option.java
@@ -0,0 +1,21 @@
+package com.ccssoft.option;
+
+import java.util.Optional;
+
+/**
+ * @Description:
+ * @Author: Administrator
+ * @CreateDate: 2018/9/11 9:56
+ * @UpdateDate: 2018/9/11 9:56
+ * @Version: 1.0
+ */
+public class Option {
+
+  public static void main(String[] args) {
+    //
+    Optional<String> str=Optional.ofNullable(null);
+    System.out.println(str.isPresent());
+   str.ifPresent(e->System.out.println(e));
+    System.out.println( str.orElse("没有值"));
+  }
+}
