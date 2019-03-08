b8154f3a011f10825ca91a61dc6fdafe770319b9
wangchengchao <1176652520@qq.com>
2019-03-08 18:37:48 +0800

    add new test

--- config-server/src/test/java/com/ccssoft/configserver/ConfigServerApplicationTests.java
+++ config-server/src/test/java/com/ccssoft/configserver/ConfigServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ccssoft.webservice;
+package com.ccssoft.configserver;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class WebserviceApplicationTests {
+public class ConfigServerApplicationTests {
 
   @Test
   public void contextLoads() {}
