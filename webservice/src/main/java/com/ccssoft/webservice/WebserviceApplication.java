857c969db7b5ab22f76d297f84237c95ddb7a353
wangchengchao <1176652520@qq.com>
2019-03-08 18:32:12 +0800

    add

--- webservice/src/main/java/com/ccssoft/webservice/WebserviceApplication.java
+++ webservice/src/main/java/com/ccssoft/webservice/WebserviceApplication.java
@@ -2,6 +2,7 @@ package com.ccssoft.webservice;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class WebserviceApplication {
 
   @Autowired Send send;
+  
+  @Value("${user.info.name}")
+  private String name;
+  
+  @Value("${user.info.age}")
+  private String age;
 
   public static void main(String[] args) {
     SpringApplication.run(WebserviceApplication.class, args);
@@ -21,5 +28,17 @@ public class WebserviceApplication {
   @GetMapping("/invoke")
   public String invoke() {
     return send.send2Pboss();
+  }
+  
+  @GetMapping("/name")
+  public String getName() {
+    
+    return this.name;
+  }
+  
+  @GetMapping("/age")
+  public String getAge() {
+    
+    return this.age;
   }
 }
