179358f0b06a89276445442fc2a8b50813b0be99
wangchengchao <1176652520@qq.com>
2019-03-11 16:09:34 +0800

    config

--- webservice/src/main/java/com/ccssoft/webservice/WebserviceApplication.java
+++ webservice/src/main/java/com/ccssoft/webservice/WebserviceApplication.java
@@ -10,12 +10,14 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 @Slf4j
+@RefreshScope
 public class WebserviceApplication {
 
   @Autowired
